997eec2fed533cfe466e695caf3b31bf9db82c3b
Navin <navinagarwalla888@gmail.com>
2018-07-02 17:51:07 +0530

    testing

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.post('/errors', (req, res) => {
 app.post('/contact-info', (req, res) => {
     console.log('[POST] /contact-info');
     const memory = req.body.conversation.memory;
-    console.log(memory.state);
-
+    
     const state= memory.state;
     const district = memory.district;
     contactinfo(state,district).then((data)=>{
